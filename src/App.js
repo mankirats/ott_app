9a132076495df29876f8b8a82800d10f9a7f317f
Mankirat <mankirat95@gmail.com>
2022-05-23 22:19:18 +0530

    Carousel component added and minor changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
 import Header from "./components/Header";
+import HomeCarosuel from "./components/HomeCarosuel";
 function App() {
     return (
         <div className="App">
             <Router>
                 <Header></Header>
+                <HomeCarosuel></HomeCarosuel>
                 <Switch>
                     <Route exact path="/">
                         <Login></Login>
